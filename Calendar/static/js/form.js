69fee30a44a8f44a4fefbc1f134c794c5e72e160
Miguel Angel Rodriguez <Marodriguez148@gmail.com>
2018-06-21 23:14:15 -0400

    Added jquery functionality to all button days

--- Calendar/static/js/form.js
+++ Calendar/static/js/form.js
@@ -58,5 +58,34 @@ $(document).ready(function(){
     });
   });
 
+  $("form[id='3']").on('submit', function(event){
+    event.preventDefault();
+    $.ajax({
+      data : {
+        event_title : $('form[id="3"]').find('input[id="event_title"]').val(),
+        start_time : $('form[id="3"]').find('select[id="start_time"]').val(),
+        end_time : $('form[id="3"]').find('select[id="end_time"]').val(),
+        event_desc : $('form[id="3"]').find('textarea[id="event_description"]').val(),
+        event_date : $('form[id="3"]').find('input[id="event_date"]').val()
+      },
+      type : 'POST',
+      url : '/events',
+    })
+    .done(function(data){
+      if(data.error) {
+        $('#errorAlert').text(data.error).show();
+      }
+      else {
+        $("#collapseEvents" + data.event_date + " > .card").append("<br>");
+        $("#collapseEvents" + data.event_date + " > .card").append("<p>Title: " + data.event_title + "</p>");
+        $("#collapseEvents" + data.event_date + " > .card").append("<p> Start Time: " + data.start_time + "</p>");
+        $("#collapseEvents" + data.event_date + " > .card").append("<p> End Time: " + data.end_time + "</p>");
+        $("#collapseEvents" + data.event_date + " > .card").append("<p> Description: " + data.event_desc + "</p>");
+        $("form[id='3']").hide();
+        $("#errorAlert").hide();
+      }
+    });
+  });
+
 
 });
